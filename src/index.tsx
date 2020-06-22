4f072ffd614c0249e364f975bd41a11477319a24
Alexander Olshanskiy <sander2411ukr@gmail.com>
2020-06-22 09:15:14 +0300

    add rules from webpack and babel

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default function App(): JSX.Element {
+  return <div></div>;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
